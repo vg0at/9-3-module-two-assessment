05594cd179a79613fc07e82d8e2544e679d6ab76
vg0at <cameronmedina0130@gmail.com>
2023-01-18 13:36:25 -0500

    Adds movie titles to select element in #titles.

--- main.js
+++ main.js
@@ -7,7 +7,28 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
-    console.log(section)
+    const BASE_URL = "https://resource-ghibli-api.onrender.com/films"
+    fetch(BASE_URL)
+        .then((response) => response.json())
+        .then((json) => {
+            // Adds movie titles to select element
+            json.forEach(element => {
+                const option = document.createElement("option");
+                option.textContent = element.title;
+                option.setAttribute("value", element.id);
+                const titles = document.querySelector("#titles");
+                titles.append(option)
+                console.log(element.title);
+            });// end adding movies to select element
+            
+            console.log(json);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+// .then((response) => {
+//     response.json
+// })
  // Add code you want to run on page load here
 }
 
